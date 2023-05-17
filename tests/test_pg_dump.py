bba1215265e0e50c777b040fd8a1729a5c9dfc6e
jkeifer <jkeifer@element84.com>
2023-05-17 14:18:31 -0700

    better pg_dump testing

--- tests/test_pg_dump.py
+++ tests/test_pg_dump.py
@@ -3,14 +3,25 @@ import pytest
 from dbami.db import pg_dump
 
 
+def remove_versions(dump: str):
+    return "\n".join(
+        [
+            line
+            for line in dump.splitlines()
+            if not (
+                line.startswith("-- Dumped from database version")
+                or line.startswith("-- Dumped by pg_dump version")
+            )
+        ]
+    )
+
+
 @pytest.mark.asyncio
-async def test_dump(tmp_db) -> None:
+async def test_pg_dump(tmp_db) -> None:
     empty_dump: str = """--
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
--- Dumped by pg_dump version 15.2
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -26,8 +37,55 @@ SET row_security = off;
 --
 -- PostgreSQL database dump complete
 --
-
 """
     rc, dump = await pg_dump("-d", tmp_db)
     assert rc == 0
-    assert dump == empty_dump
+    assert remove_versions(dump) == empty_dump
+
+
+@pytest.mark.asyncio
+async def test_pg_dump_custom_path(tmp_db) -> None:
+    empty_dump: str = """--
+-- PostgreSQL database dump
+--
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+"""
+    rc, dump = await pg_dump(
+        "compose",
+        "exec",
+        "postgres",
+        "pg_dump",
+        "-U",
+        "postgres",
+        "-d",
+        tmp_db,
+        pg_dump="docker",
+    )
+    assert rc == 0
+    assert remove_versions(dump) == empty_dump
+
+
+@pytest.mark.asyncio
+async def test_pg_dump_bad_path(tmp_db) -> None:
+    with pytest.raises(FileNotFoundError) as exc_info:
+        await pg_dump(
+            "-d",
+            tmp_db,
+            pg_dump="/something/that/does/not/exist",
+        )
+    assert str(exc_info.value).startswith("pg_dump could not be located: ")
